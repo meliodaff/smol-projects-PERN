2f38015de4a569f5ecbb72953f1cf8cd0f96bfe6
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-06-10 21:00:58 +0800

    hide allowed frontend

--- MovieReviewApp/server/index.ts
+++ MovieReviewApp/server/index.ts
@@ -10,11 +10,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://movie-app-review-jeyv.netlify.app",
-      "https://lil-cozy.netlify.app",
-    ],
+    origin: process.env.ALLOWED_FRONTEND?.split(""),
   })
 );
 
