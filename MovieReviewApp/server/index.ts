c0864732d20479c4c06ce37fc492960c023acbc6
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-06-10 21:04:07 +0800

    i forgot to add ',' in my split method

--- MovieReviewApp/server/index.ts
+++ MovieReviewApp/server/index.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.ALLOWED_FRONTEND?.split(""),
+    origin: process.env.ALLOWED_FRONTEND?.split(","),
   })
 );
 
