9da6a53f6e46e7e24e48d85781bb1c0a6063c461
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-06-10 14:51:40 +0800

    add https://movie-app-review-jeyv.netlify.app/ in my cors configuration

--- MovieReviewApp/server/index.ts
+++ MovieReviewApp/server/index.ts
@@ -10,7 +10,10 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://movie-app-review-jeyv.netlify.app/",
+    ],
   })
 );
 
